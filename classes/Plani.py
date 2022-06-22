8d700eb9d955ba4b88282b3b3db52d94b081d417
roigagusti <gutiroma@hotmail.com>
2022-06-22 16:54:22 +0200

    Planning 0.4

--- classes/Plani.py
+++ classes/Plani.py
@@ -1,24 +1,41 @@
 from this import s
 import requests
 import json
+import time
+import datetime
 
 #user: Agustí
 #app: Airtable
 class Task():
-    def __init__(self, atid, iniciativa, squad, pec_id, owner, tarea):
+    def __init__(self, atid, iniciativa, squad, pec_id, owner, tarea, start, end):
         self.atid = atid
         self.iniciativa = iniciativa
         self.squad = squad
         self.pec_id = pec_id
         self.owner = owner
         self.tarea = tarea
+        self.start = start
+        self.end = end
+class Sprint():
+    def __init__(self, atid, date, dateRaw, quarter, year, week, sprint):
+        self.atid = atid
+        self.date = date
+        self.dateRaw = dateRaw
+        self.quarter = quarter
+        self.year = year
+        self.week = week
+        self.sprint = sprint
+
+def timestampDate(date):
+    data = time.mktime(datetime.datetime.strptime(date, "%Y-%m-%d").timetuple())
+    return data
 
 class Tasques():
     def __init__(self):
         self.token = 'keyVBM9rXFpJNDEIU'
         self.base_id = 'applVLtXI9jhnWeIB'
 
-    def list(self):
+    def listTasks(self):
         url = 'https://api.airtable.com/v0/applVLtXI9jhnWeIB/initiatives'
         rest = url
         
@@ -28,15 +45,7 @@ class Tasques():
         data = response.json()
         records = []
         
-        for record in data['records']:
-            if 'iniciativa' in record['fields']:
-                iniciativa = record['fields']['iniciativa']
-            else:
-                iniciativa = '-'
-            if 'squad' in record['fields']:
-                squad = record['fields']['squad']
-            else:
-                squad = '-'
+        for record in data['records']:            
             if 'pec_id' in record['fields']:
                 pec_id = record['fields']['pec_id']
             else:
@@ -45,20 +54,52 @@ class Tasques():
                 owner = record['fields']['owner']
             else:
                 owner = '-'
-            if 'tarea' in record['fields']:
-                tarea = record['fields']['tarea']
+            if 'start_date' in record['fields']:
+                start = record['fields']['start_date'][0]
             else:
-                tarea = '-'
+                start = '-'
+            if 'end_date' in record['fields']:
+                end = record['fields']['end_date'][0]
+            else:
+                end = '-'
             
             
             tasca = Task(
                 record['id'],
-                iniciativa,
-                squad,
+                record['fields']['iniciativa'],
+                record['fields']['squad'],
                 pec_id,
                 owner,
-                tarea
+                record['fields']['tarea'],
+                timestampDate(start),
+                timestampDate(end)
             )
             records.append(tasca)
-            
+        
+        records.sort(key=lambda x: x.iniciativa)    
+        return records
+
+    def listSprints(self,year):
+        parameter = "Year"
+        url = "https://api.airtable.com/v0/applVLtXI9jhnWeIB/sprints?filterByFormula={" + parameter + "}='" + str(year) + "'"
+        rest = url
+        
+        key = 'Bearer '+ self.token
+        header = {'Authorization' : key}
+        response = requests.get(rest,headers=header)
+        data = response.json()
+        records = []
+        
+        for record in data['records']:            
+            sprint = Sprint(
+                record['id'],
+                record['fields']['date'],
+                timestampDate(record['fields']['date']),
+                record['fields']['Quarter'],
+                record['fields']['Year'],
+                record['fields']['Week'],
+                record['fields']['Sprint']
+            )
+            records.append(sprint)
+        records.sort(key=lambda x: x.date)
         return records
