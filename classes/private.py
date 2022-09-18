ec933ccb3bf255566d294cea43eff3c44423186b
roigagusti <gutiroma@hotmail.com>
2022-09-18 16:13:11 +0000

    cache from server

---
+++ classes/private.py
@@ -0,0 +1,14 @@
+# Airtable credentials
+def atCredentials():
+    token = 'keyVBM9rXFpJNDEIU'
+    base_id = 'appd2t4eco5DcWYGI'
+    return token,base_id
+
+# Database RT credentials
+def dbCredentials():
+    host = 'dev-rompetechos-cluster.cluster-cibamr7b9vqj.eu-central-1.rds.amazonaws.com'
+    database = 'dev_rt_support'
+    user = 'dev_rt_admin'
+    password = 'rtDevBuPla3ADM!'
+    port = '3306'
+    return host,database,user,password,port
